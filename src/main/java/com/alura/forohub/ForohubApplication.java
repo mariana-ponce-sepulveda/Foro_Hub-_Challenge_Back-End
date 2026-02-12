2fe0707f5f236ca6802b58ad1017aa07adbe3586
Mariana Ponce Sepúlveda <52932892+mariana-ponce-sepulveda@users.noreply.github.com>
2026-02-11 21:33:28 -0300

    Update ForohubApplication.java

--- src/main/java/com/alura/forohub/ForohubApplication.java
+++ src/main/java/com/alura/forohub/ForohubApplication.java
@@ -1,13 +1,17 @@
-package com.andromeda.forohub;
+package com.alura.forohub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Clase principal de la aplicación ForoHub.
+ * 
+ * Punto de entrada del sistema.
+ */
 @SpringBootApplication
 public class ForohubApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ForohubApplication.class, args);
 	}
-
 }
